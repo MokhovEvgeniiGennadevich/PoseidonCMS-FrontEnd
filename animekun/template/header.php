0976c6310cfe6e2b12c91e9a72cea844ba104903
MokhovEvgeniiGennadevich <git@mokhov-evgenii-gennadevich.com>
2022-07-20 15:30:51 -0400

    moved javascript from header

--- animekun/template/header.php
+++ animekun/template/header.php
@@ -5,8 +5,6 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
     <title>Document</title>
-    <script src="/js/crypto-js.min.js" integrity="sha512-E8QSvWZ0eCLGk4km3hxSsNmGWbLtSCSUcewDQPQWZF6pEU8GlT8a5fF32wOl1i8ftdMhssTrF/OhyGWwonTcXA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-    <script src="/js/app.js"></script>
-    <link rel="stylesheet" href="/css/style.css">
+    <link rel="stylesheet" href="/css/style.css?<?=$version?>">
 </head>
 <body>
