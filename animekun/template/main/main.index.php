51b2aab806213d7809b0ab190fe9d5ff64d0b4f6
MokhovEvgeniiGennadevich <git@mokhov-evgenii-gennadevich.com>
2022-07-20 15:32:18 -0400

    moved main template to main folder

---
+++ animekun/template/main/main.index.php
@@ -0,0 +1,15 @@
+<?php include('../template/header.php'); ?>
+<?php include('../template/elements/menu.top.php'); ?>
+<div class="wrapper">
+    <?php include('../template/menu_top.php'); ?>
+
+    <div class="container">
+        <h1>AnimeKun.Ru Refactoring</h1>
+        <p>Test test</p>
+
+        <div class="">
+
+        </div>
+    </div>
+</div>
+<?php include('../template/footer.php'); ?>
