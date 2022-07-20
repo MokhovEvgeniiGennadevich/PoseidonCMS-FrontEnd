d243b934b2fdbeab6cf9e0e3d353b03aa82d1157
MokhovEvgeniiGennadevich <git@mokhov-evgenii-gennadevich.com>
2022-07-20 15:29:57 -0400

    advanced routing

--- animekun/public_html/index.php
+++ animekun/public_html/index.php
@@ -1,14 +1,30 @@
 <?php
+###########################################################################
+### Security
+
+define('__SECURITY_CHECK__', true);
+
+ini_set('error_reporting', E_ALL);
+error_reporting(E_ALL);
+
+
+###########################################################################
+### Includes
+
 include('../config.php');
 include('../../apikun/functions.php');
+include('../../system/routing.web.php');
+include('../../system/functions.language.php');
+include('../language/ru.php');
+include('../language/en.php');
 
+###########################################################################
+### Routing
 
-if ( $_GET['a'] === 'user_login' ) {
-    include('../template/user_login.php');
-}
-elseif ( $_GET['a'] === 'user_register' ) {
-    include('../template/user_register.php');
-}
-else {
-    include('../template/index.php');
+if (       $site_get['m'] === 'user_login' ) {
+    include('../template/user/user.login.php');
+} elseif ( $site_get['m'] === 'user_register' ) {
+    include('../template/user/user.register.php');
+} else {
+    include('../template/main/main.index.php');
 }
