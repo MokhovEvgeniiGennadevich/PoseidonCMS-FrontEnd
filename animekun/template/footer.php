b4723a011d9452132945bb6fa2b98b5653bf1160
MokhovEvgeniiGennadevich <git@mokhov-evgenii-gennadevich.com>
2022-07-20 15:30:30 -0400

    some javascript and page generation time

--- animekun/template/footer.php
+++ animekun/template/footer.php
@@ -1,2 +1,5 @@
+<script src="/js/crypto-js.min.js?<?=$version?>" integrity="sha512-E8QSvWZ0eCLGk4km3hxSsNmGWbLtSCSUcewDQPQWZF6pEU8GlT8a5fF32wOl1i8ftdMhssTrF/OhyGWwonTcXA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script src="/js/app.js?<?=$version?>"></script>
+<p><?=__lang('page_generated'); ?>: <?php echo number_format((float)(microtime(true) - $_SERVER["REQUEST_TIME_FLOAT"]) * 1000, 2, '.', ''); ?></p>
 </body>
 </html>
